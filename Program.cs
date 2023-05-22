66c581792030d2320f0a77e2170ae0824dad55a2
Madina <ozdemirov24050@mail.ru>
2023-05-22 14:47:57 +0200

    dz68

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string num(int a)
+{
+if (a>1) return $"{a}, "+num(a-1);
+return "1";
+}
+Console.WriteLine(num(5));
